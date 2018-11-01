9073e5504f9c43c481cac07bb26e9f4fa7f7cb14
khanfrd <khanfrd94@gmail.com>
2018-11-01 22:23:38 +0530

    beryllium: Add BOARD_ROOT_EXTRA_FOLDERS/SYMLINKS
    Change-Id: Id42015f7f24640800d8af3f6d62370318434b190

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,8 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := beryllium_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
+BOARD_ROOT_EXTRA_FOLDERS := odm
+BOARD_ROOT_EXTRA_SYMLINKS := /vendor/dsp:/dsp /vendor/firmware_mnt:/firmware /vendor/bt_firmware:/bt_firmware /mnt/vendor/persist:/persist
 
 # Platform
 #TARGET_BOARD_PLATFORM := sdm845
