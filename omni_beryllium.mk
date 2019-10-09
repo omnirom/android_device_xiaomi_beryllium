ad8541d6c6c4dac41ff96e4f217599e1ca3aad34
khanfrd <khanfrd94@gmail.com>
2019-10-09 21:54:14 +0000

    beryllium: Update fingerprint props
    Change-Id: Iac843b311077da8129795274f993366225b0590f

--- omni_beryllium.mk
+++ omni_beryllium.mk
@@ -31,13 +31,15 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 PRODUCT_NAME := omni_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := POCO F1
+PRODUCT_MODEL := POCOPHONE F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.2.3.0.PEJMIXM:user/release-keys" \
-    PRIVATE_BUILD_DESC="beryllium-user 9 PKQ1.180729.001 V10.2.3.0.PEJMIXM release-keys" \
-    PRODUCT_NAME="beryllium" \
-    TARGET_DEVICE="beryllium"
+TARGET_DEVICE := beryllium
+PRODUCT_SYSTEM_NAME := beryllium
+
+VENDOR_RELEASE := 10/QKQ1.190828.002/V11.0.4.0.QEJMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/beryllium/beryllium:$(VENDOR_RELEASE)
+OMNI_BUILD_FINGERPRINT := Xiaomi/beryllium/beryllium:$(VENDOR_RELEASE)
+OMNI_PRIVATE_BUILD_DESC := "'beryllium-user 9 QKQ1.190828.002 V11.0.4.0.QEJMIXM release-keys'"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
